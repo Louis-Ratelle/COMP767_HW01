1380ef516f50e0abd6598ea7f0c94520b5cf4f8e
Michael Benitah <michael.benitah@umontreal.ca>
2020-01-25 22:09:54 -0500

    Question 2 implemented, apparently not working.

--- HW01Q02.py
+++ HW01Q02.py
@@ -1,48 +1,41 @@
 import numpy as np
 import gym
 
-env = gym.make('FrozenLake-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-
-for i in range(env.observation_space.n):
-    print('observation_space: {}'.format(i))
-print('action_space: {}'.format(env.action_space))
-env.close()
-
 
 class Policy():
-    def __init__(self, env, tol=1e-6):
+    def __init__(self, env, gamma=1, tol=1e-6):
         self.env = env
+        self.gamma = gamma
         self.tol = tol
+
+        self.counter = 0
 
         if not isinstance(env.observation_space, gym.spaces.Discrete):
             raise NotImplementedError
 
-        for s in range(env.observation_space.n):
-            self.V[s] = 0
-            self.pi[s] = env.action_space.sample()
-            self.p[s1, r, s, a] = 0        # TODO
+        self.V = np.zeros(env.observation_space.n)
+        self.pi = np.zeros(env.observation_space.n, dtype=int)
 
-    def eval(self):
+        for s in range(env.observation_space.n):
+            self.pi[s] = env.action_space.sample()
+
+    def eval(self, bValueIteration=False):
         '''Evaluates the policy value for each state s in
         env.observation_space'''
         delta = np.infty
+        i = 0
 
+        self.counter += 1
         while delta > self.tol:
             delta = 0
+            i += 1
             for s in range(self.env.observation_space.n):
                 V_old = self.V[s]
-                self.V[s] = np.sum(self.p[s1, r, s, self.pi[s]] * (r + gamma * V(s1))) for (s1, r) in       # TODO
+                self.V[s] = self._getvalue(s, self.pi[s])
                 delta = max(delta, np.abs(V_old - self.V[s]))
+        print('Policy evaluation #{} completed in {} steps.\n\nV: {}\n'.format(self.counter, i, self.V))
+        self.iterate()
+        return self.V, self.pi
 
     def iterate(self):
         '''Iterates policy evaluation to find an optimal policy and
@@ -58,12 +51,21 @@ class Policy():
         stable = True
         for s in range(self.env.observation_space.n):
             old_action = self.pi[s]
-            self.pi[s] = 0           # TODO
+
+            values = []
+            for action in range(self.env.action_space.n):
+                values.append(self._getvalue(s, action))
+
+            self.pi[s] = np.argmax(values)
+            # self.pi[s] = np.argmax([self._getvalue(s, action) for action in range(self.env.action_space.n)])
+
+            # print('state {} : {}'.format(s, values))
+
             if old_action != self.pi[s]:
                 stable = False
-        if stable:
-            return self.V, self.pi
-        else:
+        print('pi: {}'.format(self.pi))
+
+        if not stable:
             self.eval()
 
     def value_iteration(self):
@@ -71,9 +73,99 @@ class Policy():
         only one sweep (one update of each state) of policy evaluation.
 
         output: an estimation of the optimal policy'''
-        self.eval()
+        # self.eval()
+
+        # for s in range(self.env.observation_space.n):
+        #     self.pi[s] = np.argmax([self._getvalue(s, action) for action in self.env.action_space])
+        delta = np.infty
+        i = 0
+
+        while delta > self.tol:
+            delta = 0
+            i += 1
+            for s in range(self.env.observation_space.n):
+                V_old = self.V[s]
+                self.V[s] = np.max([self._getvalue(s, action) for action in range(self.env.action_space.n)])
+                delta = max(delta, np.abs(V_old - self.V[s]))
 
         for s in range(self.env.observation_space.n):
-            self.pi[s] = np.argmax()     # TODO
+            self.pi[s] = np.argmax([self._getvalue(s, action) for action in range(self.env.action_space.n)])
 
-        return self.pi
+        print('Finished value iteration in {} steps.'.format(i))
+
+        return self.V, self.pi
+
+
+    def _getvalue(self, state, action):
+        '''For a given state and action, returns the value of that
+        state according to the current policy iteration.'''
+
+        # for a given state and action, P[state][action] returns a list of
+        # tuples in the form (p, s1, r, b) containing respectively the
+        # probability, state, return and boolean for all possible states s1
+        # originating from s. The boolean determines if the state s1 is
+        # terminal or not.
+        p, s1, r, _ = zip(*self.env.P[state][action])
+
+        # convert tuples to arrays
+        p = np.array(p)
+        s1 = np.array(s1, dtype=int)
+        r = np.array(r)
+        # b = np.array(b, dtype=bool)
+
+        return np.sum(p * (r + self.gamma * self.V[s1]))
+
+
+def render_policy(env, pi):
+    print('=' * 80)
+    print('Initial state')
+    print('=' * 80)
+    env.render()
+
+    cumulative_reward = 0
+    for i, action in enumerate(pi):
+        print('=' * 80)
+        print('Step {}'.format(i+1))
+        print('=' * 80)
+        observation, reward, done, info = env.step(action)
+        cumulative_reward += reward
+        env.render()
+    print('Cumulative reward: {}'.format(cumulative_reward))
+    env.close()
+
+
+def main():
+    # env = gym.make('FrozenLake-v0')
+    env = gym.make('FrozenLake8x8-v0')
+    # env = gym.make('Taxi-v3')
+    env.reset()
+
+    pol = Policy(env, gamma=0.9)
+    V, pi = pol.eval()
+    print('Done. ****************')
+
+    valuepol = Policy(env, gamma=0.9)
+    V_val, pi_val = valuepol.value_iteration()
+    print('V: {}\n\npi:{}'.format(V_val, pi_val))
+
+    render_policy(env, pi_val)
+    print(pi==pi_val)
+
+
+if __name__ == '__main__':
+    main()
+
+# for i_episode in range(20):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         print(observation)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+
+# for i in range(env.observation_space.n):
+#     print('observation_space: {}'.format(i))
+# print('action_space: {}'.format(env.action_space))
